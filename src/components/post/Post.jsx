9cc455b717c9a45df3f4063a2d03a320c1d93d08
adecodess <ademosuu@gmail.com>
2021-07-08 08:45:41 +0100

    completed all components

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -15,8 +15,20 @@ export default function Post() {
                       <MoreVert/>
                   </div>
               </article>
-              <article className="PostCenter"></article>
-              <article className="PostBottom"></article>
+              <article className="PostCenter">
+                  <span className="postText">Hey its my first post :)</span>
+                  <img src="/asset/post/post1-unsplash.jpg" alt="post" className="postImage" />
+              </article>
+              <article className="postBottom">
+                  <div className="postBottomLeft">
+                      <img className="likeIcon" src="asset/like.png" alt="like" />
+                      <img className="likeIcon" src="asset/heart.png" alt="love" />
+                      <span className="postLikeCounter">32 people like it</span>
+                  </div>
+                  <div className="postBottomRight">
+                      <span className="postCommentText">9 comments</span>
+                  </div>
+              </article>
           </div>
         </main>
     )
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,8 +2,52 @@ import "./rightbar.css"
 
 export default function Rightbar() {
     return (
-        <div className="rightbar">
-            this is the right bar
-        </div>
+        <main className="rightbar">
+           <article className="rightbarWrapper">
+               <div className="birthdayContainer">
+                   <img src="asset/gift.png" alt="gift" className="birthdayImg" />
+                   <span className="birthdayText"> <b>Jane Doe</b> and <b>3  other friends</b> have birthday today</span>
+               </div>
+               <img src="asset/ad.jpg" alt="advert" className="rightbarAd" />
+               <h4 className="rightbarTitle">Online Friends</h4>
+               <ul className="rightbarFriendList">
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img src="/asset/person/person4-unsplash.jpg" alt="rightbarprofile" className="rightbarProfileImg" />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className=" ">John Doe</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img src="/asset/person/person4-unsplash.jpg" alt="rightbarprofile" className="rightbarProfileImg" />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className=" ">John Doe</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img src="/asset/person/person4-unsplash.jpg" alt="rightbarprofile" className="rightbarProfileImg" />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className=" ">John Doe</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img src="/asset/person/person4-unsplash.jpg" alt="rightbarprofile" className="rightbarProfileImg" />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className=" ">John Doe</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img src="/asset/person/person4-unsplash.jpg" alt="rightbarprofile" className="rightbarProfileImg" />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className=" ">John Doe</span>
+                   </li>
+               </ul>
+           </article>
+        </main>
     )
 }
